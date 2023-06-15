867e3c7699faa9afefed4affce88a5256755d76b
Ahmed Yahya <a.yahya1@yahoo.com>
2023-06-15 10:50:00 +0300

    Minor changes

--- admin/connect.php
+++ admin/connect.php
@@ -2,9 +2,10 @@
 // Database Connection
 
 // $dsn  = 'mysql:host=localhost;dbname=shop'; // Data Source Name
-$dsn  = 'mysql:host=localhost;port=3306;dbname=shop'; // Data Source Name    // Specifying the MySQL Port Number
+// $dsn  = 'mysql:host=localhost;port=3306;dbname=shop'; // Data Source Name    // Specifying the MySQL Port Number
+$dsn  = 'mysql:host=localhost;dbname=shop'; // Data Source Name    // Specifying the MySQL Port Number
 $user = 'root';
-$pass = '0000';
+$pass = '';
 $options = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' 
 );
